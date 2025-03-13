f92fd75679dd17654e7e62d8bf61f3434f5a4542
arwell <202311160016@mail.bnu.edu.cn>
2025-03-13 11:02:10 +0800

    change the configs and add a figure zoom plugin

---
+++ docs/.vitepress/configs/nav.ts
@@ -0,0 +1,6 @@
+/* configs/nav.ts */
+import type { DefaultTheme } from 'vitepress'
+
+export const nav: DefaultTheme.Config['nav'] = [
+  { text: 'Home', link: '/' },
+]
---
+++ docs/.vitepress/configs/sidebar.ts
@@ -0,0 +1,119 @@
+/* configs/sidebar.ts */
+import type { DefaultTheme } from 'vitepress'
+
+export const sidebar: DefaultTheme.Config['sidebar'] = {
+  '/blog/': [
+  {
+    text: 'arwell\'s blog',
+    items: [
+      // { text: '首页', link: '/blog/index' }
+    ]
+  },
+  {
+    text: 'math',
+    collapsed: false,
+    items: [
+      { text: '正交曲面坐标系', link: '/blog/math/metric' },
+      { text: '向量微分算符', link: '/blog/math/nabla' },
+      { text: '全反对称三秩张量', link: '/blog/math/antisymmetric_tensor'},
+      { text: '概统', link: 'blog/math/probability_and_mathematical_statistics'},
+      { text: '概率论中的特征函数', link: 'blog/math/characteristic_function'}
+    ]
+  },
+  {
+    text: 'physics',
+    collapsed: false,
+    items: [
+      { text: '电偶极子和磁偶极子', link: '/blog/physics/dipoles' },
+      { text: '接地问题', link: '/blog/physics/earthing'},
+      { text: '光学', link: '/blog/physics/optics'},
+      { text: '原子物理', link: '/blog/physics/atom'},
+      { text: '理论力学', link: '/blog/physics/analytial_mechanics'}
+    ]
+  },
+  {
+    text: 'astronomy',
+    collapsed: false,
+    items: [
+      { text: '天文测量基础', link: '/blog/astronomy/measurement'}
+    ]
+  },
+  {
+    text: 'tools',
+    collapsed: false,
+    items: [
+      { text: 'python数据处理', link: '/blog/tools/python'},
+      { text: 'python虚拟环境', link: '/blog/tools/python-venv'},
+      { text: 'make的使用', link: '/blog/tools/make'},
+      { text: 'git命令', link: '/blog/tools/git'},
+      { text: 'docker命令', link: '/blog/tools/docker'},
+      { text: 'yazi的使用', link: '/blog/tools/yazi'},
+      { text: 'vimium的使用', link: '/blog/tools/vimium'},
+      { text: 'wps问题解答', link: '/blog/tools/wps'}
+    ]
+  }
+],
+
+'/hw/': [
+  {
+    text: '韦境量\'s作业',
+    items: [
+      { text: '用python绘制本征函数系拟合图', link: '/hw/fit'},
+      { text: 'delta函数展开', link: '/hw/fit_delta'}
+    ]
+  }
+],
+
+'/exam/': [
+  {
+    text: 'arwell',
+    items: [
+      { text: 'python期末考试', link: '/exam/py_exam'}
+    ]
+  }
+],
+
+'/web_security/': [
+  {
+    text: 'arwell',
+    items: [
+      { text: '首页', link: '/web_security/index'}
+    ]
+  },
+  {
+    text: '基础入门',
+    collapsed: false,
+    items: [
+      { text: '网络安全导论', link: '/web_security/basic/intro' },
+      { text: '渗透测试基础', link: '/web_security/basic/penetration_testing'},
+      { text: '计算机网络基础', link: '/web_security/basic/network'},
+      { text: '操作系统基础', link: '/web_security/basic/operating_system'},
+      { text: 'Web基础', link: '/web_security/basic/web'},
+      { text: '数据库基础', link: '/web_security/basic/databases'},
+      { text: 'php编程基础', link: '/web_security/basic/php'}
+    ]
+  },
+  {
+    text: '技术进阶',
+    collapsed: false,
+    items: [
+      { text: '弱口令与口令爆破', link: '/web_security/vulnerability/weak_password'},
+      { text: 'XSS漏洞', link: 'web_security/vulnerability/xss'},
+      { text: 'CSRF漏洞', link: 'web_security/vulnerability/csrf'},
+      { text: 'SSRF漏洞', link: 'web_security/vulnerability/ssrf'},
+      { text: 'XXE漏洞', link: 'web_security/vulnerability/xxe'},
+      { text: 'Python', link: 'web_security/vulnerability/python'}
+    ]
+  },
+  {
+    text: '附录',
+    collapsed: false,
+    items: [
+      { text: 'ifconfig', link: '/web_security/appendix/ifconfig'},
+      { text: 'win靶机配置', link: '/web_security/appendix/install_win'},
+      { text: 'ufw命令', link: '/web_security/appendix/ufw'},
+      { text: 'docker安装kali', link: 'web_security/appendix/docker_kali'}
+    ]
+  }
+]
+} 
